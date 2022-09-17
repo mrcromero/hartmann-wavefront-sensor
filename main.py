bdd39805586aca46a8d5979f80b4aff7d8ca3709
mrcromero <mrcromero33@gmail.com>
2022-09-17 15:09:02 -0700

    reorganize everything to classes

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import numpy as np
-
-import src.find_centroid as fc
+from src.classes.blob import Blob
+from src.classes.grid import Grid
+from src.classes.pixelcoords import PixelCoords
+from src.classes.pixelvector import PixelVector
 
 # Tests
 if __name__ == "__main__":
     a = np.array([[1, 1, 0], [1, 0, 0], [0, 0, 0]])
-    coords = fc.find_centroid(a)
+    test_blob = Blob(a)
+    coords = test_blob.find_centroid()
     print("x coord: " + str(coords.x) + ", y coord: " + str(coords.y))
---
+++ src/classes/blob.py
@@ -0,0 +1,29 @@
+import numpy as np
+from .pixelcoords import PixelCoords
+
+# A blob is a matrix of pixels with intensity values
+
+class Blob:
+    pixel_mat = None
+
+    # pixel_mat is a NumPy array
+    def __init__(self, pixel_mat):
+        self.pixel_mat = pixel_mat
+    
+    # Finds the centroid in one NumPy array
+    #
+    # returns: a PixelCoords object containing the centroid of the blob
+    def find_centroid(self):
+        total_sum = np.sum(self.pixel_mat)
+        weights_x = np.sum(self.pixel_mat, axis=0)/total_sum
+        weights_y = np.sum(self.pixel_mat, axis=1)/total_sum
+
+        centroid_x = 0
+        for i in range(len(weights_x)):
+            centroid_x += i*weights_x[i]
+
+        centroid_y = 0
+        for i in range(len(weights_y)):
+            centroid_y += i*weights_y[i]
+
+        return PixelCoords(centroid_x, centroid_y)
