62276f634edc2ee84b980ae7fc239a0ca3bda83a
mrcromero <mrcromero33@gmail.com>
2022-09-18 19:01:27 -0700

    add the blob centroid test

--- tests/centroiding_tests.py
+++ tests/centroiding_tests.py
@@ -9,12 +9,16 @@ from src.classes.grid import Grid
 from src.classes.pixelcoords import PixelCoords
 from src.classes.pixelvector import PixelVector
 
+def test_blob_centroid():
+    a = np.array([[1, 1, 1], [1, 0, 0], [0, 0, 0]])
+    test_blob = Blob(a)
+    coords = test_blob.find_centroid()
+    error_s = "Centroid coords x: {0}, y: {1}".format(coords.x, coords.y)
+    assert coords.x == 0.75 and coords.y == 0.25, error_s
 
 
 # Tests for checking centroiding
 
 if __name__ == "__main__":
-    a = np.array([[1, 1, 0], [1, 0, 0], [0, 0, 0]])
-    test_blob = Blob(a)
-    coords = test_blob.find_centroid()
-    print("x coord: " + str(coords.x) + ", y coord: " + str(coords.y))
+    test_blob_centroid()
+    print("All tests passed!")
