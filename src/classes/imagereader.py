0c57e2bab391158f3113ae072a82bbcf559b93dc
mrcromero <mrcromero33@gmail.com>
2022-10-04 09:43:12 -0700

    add init image test

--- src/classes/imagereader.py
+++ src/classes/imagereader.py
@@ -8,6 +8,8 @@ from .grid import Grid
 from .blob import Blob
 
 class ImageReader:
+    blob_size = 0
+    grid_size = 0
     image = None
     center_x = None
     center_y = None
@@ -17,10 +19,15 @@ class ImageReader:
     # streaming works
     # Regardless, would like to have an optional 'path' variable for testing
     # single images
-    def __init__(self, path):
+    # Default values:
+    #   - for Blobs is 140x140 pixels
+    #   - for Grids is 7x7 Blobs
+    def __init__(self, path, blob_size=140, grid_size=7):
         self.image = cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2GRAY)
-        center_x = sum([i for i in range(len(self.image))])/len(self.image)
-        center_y = sum([i for i in range(len(self.image[0]))])/len(self.image[0])
+        self.center_x = len(self.image)//2
+        self.center_y = len(self.image[0])//2
+        self.blob_size = blob_size
+        self.grid_size = grid_size
         self.get_grid()
 
     def get_grid(self, x=center_x, y=center_y):
@@ -31,14 +38,17 @@ class ImageReader:
             else:
                 return self.grid
         # Calculate grid using center_x and center_y and place in grid
-        grid = Grid()
+        grid = Grid([])
         self.grid = grid
         return grid
 
     def update_grid(self, delta_x, delta_y):
+        length_to_edge = self.grid_size/2
         new_x = self.center_x + delta_x
         new_y = self.center_y + delta_y
-        if (new_x < 0 or new_x > len(self.image) or
-            new_y < 0 or new_y > len(self.image)):
+        if (new_x-length_to_edge < 0 or
+            new_x+length_to_edge > len(self.image)-1 or
+            new_y-length_to_edge < 0 or
+            new_y+length_to_edge > len(self.image)-1):
             return self.get_grid()
         return self.get_grid(new_x, new_y)
--- tests/image_tests.py
+++ tests/image_tests.py
@@ -1,9 +1,15 @@
 import sys
 import pathlib
 
-
 # setting path
 sys.path.append('/'.join(str(pathlib.Path(__file__).parent.resolve()).split('/')[:-1]))
 
+from src.classes.imagereader import ImageReader
+
+def test_init_image_reader():
+    a = ImageReader('tests/images/100.bmp')
+    assert a.get_grid() is not None
+
 if __name__ == "__main__":
+    test_init_image_reader()
     print("All image tests passed!")
