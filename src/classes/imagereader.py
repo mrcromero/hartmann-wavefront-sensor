cd2e3e097088057118c2bb147624a91d781d4582
mrcromero <mrcromero33@gmail.com>
2022-10-04 12:18:45 -0700

    add image gridding

--- src/classes/imagereader.py
+++ src/classes/imagereader.py
@@ -1,5 +1,7 @@
 import sys
 import cv2
+import math
+import numpy as np
 
 # setting path
 sys.path.append('./')
@@ -22,8 +24,8 @@ class ImageReader:
     # Default values:
     #   - for Blobs is 140x140 pixels
     #   - for Grids is 7x7 Blobs
-    def __init__(self, path, blob_size=140, grid_size=7):
-        self.image = cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2GRAY)
+    def __init__(self, path, blob_size=100, grid_size=7):
+        self.image = np.array(cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2GRAY))
         self.center_x = len(self.image[0])//2
         self.center_y = len(self.image)//2
         self.blob_size = blob_size
@@ -38,13 +40,27 @@ class ImageReader:
             else:
                 return self.grid
         # Calculate grid using center_x and center_y and place in grid
-        
-        grid = Grid([])
+        edge_dist = math.ceil((self.grid_size/2) * self.blob_size)-1
+        x_edges = [(self.center_x-edge_dist) + (self.blob_size*i) for i in range(self.grid_size+1)]
+        y_edges = [(self.center_y-edge_dist) + (self.blob_size*i) for i in range(self.grid_size+1)]
+
+        blob_array = []
+        for i in range(self.grid_size):
+            blob_array.append([])
+            for j in range(self.grid_size):
+                start_x = x_edges[j]
+                start_y = y_edges[i]
+                end_x = x_edges[j+1]
+                end_y = y_edges[i+1]
+                blob_mat = self.image[start_y:end_y, start_x:end_x]
+                blob_array[i].append(Blob(blob_mat))
+
+        grid = Grid(blob_array)
         self.grid = grid
         return grid
 
     def update_grid(self, delta_x, delta_y):
-        length_to_edge = (self.grid_size/2) * self.blob_size
+        length_to_edge = math.ceil((self.grid_size/2) * self.blob_size)
         new_x = self.center_x + delta_x
         new_y = self.center_y + delta_y
         if (new_x-length_to_edge < 0 or
