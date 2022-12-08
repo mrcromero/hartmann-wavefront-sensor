07f0d64dd622cd9e60617706ab100b7bd49969d5
mrcromero <mrcromero33@gmail.com>
2022-12-08 13:51:29 -0700

    add x and y tilt tests

--- tests/reconstruction_tests.py
+++ tests/reconstruction_tests.py
@@ -7,16 +7,44 @@ sys.path.append('/'.join(str(pathlib.Path(__file__).parent.resolve()).split('/')
 
 from src.classes.zernikesolver import ZernikeSolver
 
+x_vals = [0, 1, 2, 3]
+y_vals = [0, 1, 2, 3]
+xv_vals = [0, 1, 2, 3]
+yv_vals = [4, 5, 6, 7]
+control_c = []
+
+
 def test_zernike_solver():
-    x_vals = [0, 1, 2, 3]
-    y_vals = [0, 1, 2, 3]
-    v_vals = [0, 1, 2, 3, 4, 5, 6, 7]
+    v_vals = xv_vals+yv_vals
+    a = ZernikeSolver(x_array=x_vals, y_array=y_vals, v_array=v_vals)
+    global control_c
+    control_c = a.solve()
+    assert len(control_c) == 15, "%s coefficients found" % len(control_c)
+    
+def test_zernike_xshift():
+    print("   # Testing change in x-tilt")
+    new_xv_vals = [xv + 1 for xv in xv_vals]
+    v_vals = new_xv_vals+yv_vals
     a = ZernikeSolver(x_array=x_vals, y_array=y_vals, v_array=v_vals)
     c = a.solve()
-    for i in range(len(c)):
-        print("C" + str(i) + ": " + str(c[i]))
+    test_c = c[0] + c[2:]
+    test_control = control_c[0] + control_c[2:]
+    equalities = [test_c[i] == test_control[i] for i in range(len(test_c))]
+    assert all(equalities), "incorrect coefficients have changed"
+
+def test_zernike_yshift():
+    print("   # Testing change in y-tilt")
+    new_yv_vals = [yv + 1 for yv in yv_vals]
+    v_vals = xv_vals+new_yv_vals
+    a = ZernikeSolver(x_array=x_vals, y_array=y_vals, v_array=v_vals)
+    c = a.solve()
+    test_c = c[0] + c[2:]
+    test_control = control_c[0] + control_c[2:]
+    equalities = [test_c[i] == test_control[i] for i in range(len(test_c))]
+    assert all(equalities), "incorrect coefficients have changed"
 
 if __name__ == "__main__":
     print("### Running Reconstruction Tests ###")
     test_zernike_solver()
+    test_zernike_xshift()
     print("### All reconstruction tests passed! ###")
